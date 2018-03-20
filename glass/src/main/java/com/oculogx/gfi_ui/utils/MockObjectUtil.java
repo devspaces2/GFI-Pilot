eb107b5490b5a079bd9444cb709ed4ef85664f3b
Charu Thomas <charu.thomas@gmail.com>
2018-03-20 01:38:24 -0400

    finished demo images

--- glass/src/main/java/com/oculogx/gfi_ui/utils/MockObjectUtil.java
+++ glass/src/main/java/com/oculogx/gfi_ui/utils/MockObjectUtil.java
@@ -27,30 +27,38 @@ public class MockObjectUtil {
         //item2.primaryImageURL = "https://i.ytimg.com/vi/hEJnMQG9ev8/maxresdefault.jpg";
 
         Item item3 = new Item(410192, "______", "BLUE HEAVEN BLUE CHEESE POWDER", "24/3 OZ", 43, "10B01", 1550);
-        //item3.primaryImageURL = R.drawable.item3front;
-        //item3.secondaryImageURL = R.drawable.item3side;
+        item3.primaryImageURL = R.drawable.item3front;
+        item3.secondaryImageURL = R.drawable.item3side;
         //item3.primaryImageURL = "https://www.dollargeneral.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/0/0/00012000504082_0.jpg";
 
         Item item4 = new Item(60283, "______", "OLIVE MIX WHOLE GREEK UNPITTED", "6/5.6 OZ", 43, "14B03", 1840);
-        //item4.primaryImageURL = R.drawable.item4front;
-        //item4.secondaryImageURL = R.drawable.item4side;
+        item4.primaryImageURL = R.drawable.item4front;
+        item4.secondaryImageURL = R.drawable.item4side;
         //item4.primaryImageURL = "";
 
         Item item5 = new Item(46078, "______","HONEY STICKS BONNE MAMAN CASE", "100/0.5 OZ", 43, "20B01", 2225);
-        //item5.primaryImageURL = R.drawable.item5front;
-        //item5.secondaryImageURL = R.drawable.item5side;
+        item5.primaryImageURL = R.drawable.item5front;
+        item5.secondaryImageURL = R.drawable.item5side;
         //item5.primaryImageURL = "";
 
         Item item6 = new Item(20855, "______","CHIPS KETTLE SWEET MAUI ONION", "24/2 OZ", 44, "12A01", 2475);
+        item6.primaryImageURL = R.drawable.item6front;
+        item6.secondaryImageURL = R.drawable.item6side;
         //item6.primaryImageURL = "";
 
         Item item7 = new Item(20850, "______", "CHIPS KETTLE SALT AND VINEGAR", "24/2 OZ", 44, "22A01", 2850);
+        item7.primaryImageURL = R.drawable.item7front;
+        item7.secondaryImageURL = R.drawable.item7side;
         //item7.primaryImageURL = "";
 
         Item item8 = new Item(11776, "______", "MELBA TOAST 1.75 IN CT688", "688/1.75 IN", 45, "08A01", 4380);
+        item8.primaryImageURL = R.drawable.item8front;
+        item8.secondaryImageURL = R.drawable.item8side;
         //item8.primaryImageURL = "";
 
         Item item9 = new Item(55050, "______", "WILD MUSHROOM MIX IN PAIL", "4/3.3 LB", 45, "26A02", 5900);
+        item9.primaryImageURL = R.drawable.item9front;
+        item9.secondaryImageURL = R.drawable.item9side;
         //item9.primaryImageURL = "";
 
 
